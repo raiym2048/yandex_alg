e3b0d3913c9cbdc5c188df63981517a6ce08788f
raiym <76080041+raiym2048@users.noreply.github.com>
2024-07-09 09:23:30 +0600

    Initial commit

---
+++ src/main/java/com/example/yandex_alg/YandexAlgApplication.java
@@ -0,0 +1,13 @@
+package com.example.yandex_alg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexAlgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexAlgApplication.class, args);
+    }
+
+}
