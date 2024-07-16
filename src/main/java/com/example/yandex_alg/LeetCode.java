8817f03f31e680047a080a569a5a4be528d24823
raiym <76080041+raiym2048@users.noreply.github.com>
2024-07-16 11:17:16 +0600

    roman

--- src/main/java/com/example/yandex_alg/LeetCode.java
+++ src/main/java/com/example/yandex_alg/LeetCode.java
@@ -6,16 +6,33 @@ import java.util.Map;
 public class LeetCode {
     // 1. Two Sum
     public static void main(String[] args) {
-        System.out.println(isPalindrome(10));
+        System.out.println(romanToInt("III"));
     }
-    public static boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        for (int i = 0;i < s.length()/2;i++){
-            if (s.charAt(i) != s.charAt(s.length()-i-1)){
-                return false;
+    public static int romanToInt(String s) {
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        int x = 0;
+        for (int i = 0;i < s.length();i++){
+            if (roman.containsKey(s.charAt(i))){
+                int value =  roman.get(s.charAt(i));
+                if (i+1<s.length() && value < roman.get(s.charAt(i+1))){
+                    value = roman.get(s.charAt(i+1)) - value;
+                    x += value;
+                    i++;
+                }
+                else {
+                    x+= value;
+                }
             }
         }
-        return true;
+        return x;
     }
 
 }
